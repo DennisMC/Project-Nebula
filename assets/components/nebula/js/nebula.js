c8c04a70ac0a0a2351cdb83e0e255a62b3238351
kideh <kim@bettercollective.com>
2014-08-07 13:09:01 +0200

    Project structure

--- assets/components/nebula/js/nebula.js
+++ assets/components/nebula/js/nebula.js
@@ -1,3 +1,38 @@
-/**
- * Created by Dennis on 07-08-14.
- */
+var map;
+var marker;
+var myLatitude = 55.680206;
+var myLongitude = 12.591796;
+var locBetterCollective = new google.maps.LatLng(myLatitude, myLongitude);
+
+function toggleBounce() {
+
+    if (marker.getAnimation() != null) {
+        marker.setAnimation(null);
+    } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+}
+
+function addNewLocation(e) {
+    console.log(e);
+}
+
+function initialize() {
+    var mapOptions = {
+        zoom: 18,
+        center: locBetterCollective
+    };
+    map = new google.maps.Map(document.getElementById('mapCurrentLocation'),
+        mapOptions);
+
+    marker = new google.maps.Marker({
+        map:map,
+        draggable:true,
+        animation: google.maps.Animation.DROP,
+        position: locBetterCollective
+    });
+    google.maps.event.addListener(marker, 'click', toggleBounce);
+    google.maps.event.addListener(map, 'click', addNewLocation);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
---
+++ core/components/nebula/processors/addNewBodega.php
@@ -0,0 +1,15 @@
+<?php
+require_once MODX_CORE_PATH . 'components/nebula/model/Nebula.class.php';
+
+$objNebula = new Nebula($modx);
+
+if(array_key_exists('newBodega', $arrRequest['params'])) {
+    $arrBodegaData = json_decode($arrRequest['params']['newBodega'], true);
+
+    // This variable is json encoded as the “result” in the ajax connector
+//    $arrOutput = ->setNewBodea($arrBodegaData);
+}
+else {
+    // Return with parameters error
+    $intRpcError = -32602;
+}
